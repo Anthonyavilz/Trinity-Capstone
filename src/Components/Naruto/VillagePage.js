7dd3033f50ab4c647a3c800f8c0c26208d832dd4
Phillip Avila <Anthonyavilz@gmail.com>
2023-09-08 15:57:32 -0500

    reworking village page to not use a image map but just card tiles to route to village info pages

--- src/Components/Naruto/VillagePage.js
+++ src/Components/Naruto/VillagePage.js
@@ -1,34 +1,14 @@
-import ImageMapper from 'react-img-mapper'
+
 import { useRouter } from 'next/router'
+import worldMap from '../../../public/Images/Naruto-Images/World-Map.jpg'
 
-const VillagePage = () => {
+const VillagePage = props => {
     
-    const router = useRouter()
+    // const router = useRouter()
 
-    const URL = 'https://anbulegacy.com/wp-content/uploads/2021/05/world-map-edit.jpg'
-    const MAP = {
-        name: 'world-map',
-        areas: [
-            {
-                name: 'Rain', shape: 'rect', coords: [274,381,313,404], preFillColor: 'white', fillColor: 'Blue' 
-            },
-            {
-                name: 'Sand', shape: 'circle', coords: [219,539,11], preFillColor: 'white', fillColor: 'Blue'
-            }
-        ]
-    }
-
-    const handleRainAreaClick = (area) => {
-        if(area.name === 'Rain') {
-            router.push('/naruto/villages/rainVillage')
-        } else if (area.name === 'Sand') {
-            router.push('/naruto/villages/sandVillage')
-        }
-    }
 
     return (
         <div className='village-page' >
-            <ImageMapper src={URL} map={MAP} onClick={handleRainAreaClick}/>
         </div>
     )
 }
